0f616c743991460c3780282d3fb490ffd234e018
Tyler Long <tyler4long@gmail.com>
2020-02-21 17:26:36 -0800

    Add content to salesforce

--- content.js
+++ content.js
@@ -1,3 +1,3 @@
-console.log(document.getElementsByTagName('title')[0].textContent)
-
-document.getElementsByTagName('title')[0].textContent = 'hello world'
+const sectionHeader = document.getElementById('section_header')
+var textNode = document.createTextNode('This is my caption.')
+sectionHeader.parentNode.insertBefore(textNode, sectionHeader)
