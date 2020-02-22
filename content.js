220bca81309e289506a42ff70a3ef454e4600504
Tyler Long <tyler4long@gmail.com>
2020-02-21 17:57:15 -0800

    Try storage

--- content.js
+++ content.js
@@ -1,3 +1,22 @@
+/* global chrome  */
 const sectionHeader = document.getElementById('section_header')
 var textNode = document.createTextNode('This is my caption.')
 sectionHeader.parentNode.insertBefore(textNode, sectionHeader)
+
+chrome.storage.sync.set({ temp: 'hello' }, () => {
+})
+
+chrome.storage.sync.get('temp', dict => {
+  console.log('before')
+  console.log(dict)
+  console.log('after')
+})
+
+chrome.storage.sync.clear(() => {
+})
+
+chrome.storage.sync.get('temp', dict => {
+  console.log('before')
+  console.log(dict)
+  console.log('after')
+})
