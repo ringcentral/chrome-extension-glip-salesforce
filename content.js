5aacf00fe91c9a9545c3307a89f806e479c684c1
Tyler Long <tyler4long@gmail.com>
2020-02-26 11:27:38 -0800

    Parse and pass information

--- content.js
+++ content.js
@@ -1,17 +1,16 @@
-/* global chrome  */
 const sectionHeader = document.getElementById('section_header')
 const containerNode = document.createElement('div')
 containerNode.style.cssText = 'float: right;'
 sectionHeader.parentNode.insertBefore(containerNode, sectionHeader)
 
-chrome.storage.sync.get('RingCentralToken', dict => {
-  if (!dict.RingCentralToken) {
-    containerNode.innerText = 'No RC token'
-    chrome.storage.sync.set({ RingCentralToken: 'world' }, () => {
-    })
-  } else {
-    containerNode.innerText = 'Has RC token'
-    chrome.storage.sync.clear(() => {
-    })
-  }
-})
+const caseId = document.getElementsByClassName('pageDescription')[0].textContent.trim()
+console.log(caseId)
+const accountName = Array.from(document.getElementsByClassName('labelCol')).filter(ele => ele.textContent.trim() === 'Account Name')[0].nextSibling.textContent.trim()
+console.log(accountName)
+const subject = Array.from(document.getElementsByClassName('labelCol')).filter(ele => ele.textContent.trim() === 'Subject')[0].nextSibling.textContent.trim()
+console.log(subject)
+const urlSearchParams = new URLSearchParams()
+urlSearchParams.append('caseId', caseId)
+urlSearchParams.append('accountName', accountName)
+urlSearchParams.append('subject', subject)
+containerNode.innerHTML = `<a target="_blank" href="https://chuntaoliu.com/chrome-extension-glip-salesforce/index.html?${urlSearchParams.toString()}">Go to Glip team</a>`
