27b3b64cfc13f856925f2b5e61ed702d47260988
Tyler Long <tyler4long@gmail.com>
2020-02-27 11:01:52 -0800

    Show text to create a new team

--- src/index.js
+++ src/index.js
@@ -37,10 +37,6 @@ const saveTeams = async newTeams => {
     div.innerHTML = `<a href="${authorizeUri}">Login Glip</a>`
     document.body.appendChild(div)
   } else {
-    const div = document.createElement('div')
-    div.innerHTML = '<span>You have logged into Glip</span>'
-    document.body.appendChild(div)
-
     rc.token(token)
     try {
       await rc.get('/restapi/v1.0/account/~/extension/~')
@@ -78,7 +74,9 @@ const saveTeams = async newTeams => {
       div.innerHTML = `<span>We have found the following Glip teams:<ul>${existingTeams.map(t => `<li>${t.name} [<a href=" https://app.glip.com/chat/r?groupid=${t.id}">Open in Glip</a>] [<a href="https://jupiter.fiji.gliprc.com/messages/${t.id}">Open in Jupiter</a>]</li>`).join('')}</ul></span>`
       document.body.appendChild(div)
     } else {
-
+      const div = document.createElement('div')
+      div.innerHTML = '<span>We didn\'t find the team you requested. <a href="">Create a new team</a></span>'
+      document.body.appendChild(div)
     }
   }
   spinnerDiv.remove()
