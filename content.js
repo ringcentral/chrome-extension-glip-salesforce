9afda6e7ee8d160da6a76aae74f03ab7319f7c97
Tyler Long <tyler4long@gmail.com>
2020-09-02 09:31:12 -0700

    Work for new classic GUI

--- content.js
+++ content.js
@@ -2,12 +2,14 @@ const sectionHeader = document.getElementById('section_header')
 const containerNode = document.createElement('div')
 sectionHeader.parentNode.insertBefore(containerNode, sectionHeader)
 
-const caseId = document.getElementsByClassName('pageDescription')[0].textContent.trim()
-console.log(caseId)
-const accountName = Array.from(document.getElementsByClassName('labelCol')).filter(ele => ele.textContent.trim() === 'Account Name')[0].nextSibling.textContent.trim()
-console.log(accountName)
-const subject = Array.from(document.getElementsByClassName('labelCol')).filter(ele => ele.textContent.trim() === 'Subject')[0].nextSibling.textContent.trim()
-console.log(subject)
+let caseId = ''
+let accountName = ''
+let subject = ''
+
+caseId = document.getElementsByTagName('title')[0].text.match(/\d{6,10}/)[0]
+accountName = Array.from(document.getElementsByClassName('labelCol')).filter(ele => ele.textContent.trim() === 'Account Name')[0].nextSibling.textContent.trim()
+subject = Array.from(document.getElementsByClassName('labelCol')).filter(ele => ele.textContent.trim() === 'Subject')[0].nextSibling.textContent.trim()
+
 const urlSearchParams = new URLSearchParams()
 urlSearchParams.append('keyword', caseId)
 urlSearchParams.append('teamName', `${accountName}: Case ${caseId} ${subject}`) // Dolby Labs: Case 09681148 China/India GW solution pricing
